d3b375176bf2c28848158c3f91812d572693a979
Catherine Hilton <catherine.hilton1786@gmail.com>
2023-04-28 11:47:26 -0400

    added starter code for step one

---
+++ Guided-Practice/Homework-Block16/discount-chain.js
@@ -0,0 +1,31 @@
+//subscription = 25% discount
+//coupon = $10 discount
+
+const timmy = {
+    prescription: "acetaminophen",
+    pricePerRefill: 25,
+    refills: 3,
+    subscription: false,
+    coupon: true,
+};
+
+const sarah = {
+    prescription: "diphenhydramine",
+    pricePerRefill: 50,
+    refills: 1,
+    subscription: true,
+    coupon: false,
+}
+
+const rocky = {
+    prescription: "phenylephrine",
+    pricePerRefill: 30,
+    refills: 5,
+    subscription: true,
+    coupon: true,
+}
+//function for subscription
+
+//function for coupon
+
+//function for both subscription and coupon
