7efcad1204626a0922370feb9546e7dc7f7cd7d1
Catherine Hilton <catherine.hilton1786@gmail.com>
2023-04-28 20:45:51 -0400

    finished

--- Guided-Practice/Homework-Block16/discount-chain.js
+++ Guided-Practice/Homework-Block16/discount-chain.js
@@ -2,30 +2,69 @@
 //coupon = $10 discount
 
 const timmy = {
-    prescription: "acetaminophen",
-    pricePerRefill: 25,
-    refills: 3,
-    subscription: false,
-    coupon: true,
+  prescription: "acetaminophen",
+  pricePerRefill: 25,
+  refills: 3,
+  subscription: false,
+  coupon: true,
 };
 
 const sarah = {
-    prescription: "diphenhydramine",
-    pricePerRefill: 50,
-    refills: 1,
-    subscription: true,
-    coupon: false,
-}
+  prescription: "diphenhydramine",
+  pricePerRefill: 50,
+  refills: 1,
+  subscription: true,
+  coupon: false,
+};
 
 const rocky = {
-    prescription: "phenylephrine",
-    pricePerRefill: 30,
-    refills: 5,
-    subscription: true,
-    coupon: true,
-}
+  prescription: "phenylephrine",
+  pricePerRefill: 30,
+  refills: 5,
+  subscription: true,
+  coupon: true,
+};
+//function for total refill cost
+// function totalRefills(costPer,numRefills) {
+//     return costPer * numRefills;
+
+// };
+
 //function for subscription
+
+function subscriptionOnly(costPer, numRefills) {
+  if (sarah.subscription == true) {
+    return costPer * numRefills - costPer * numRefills * 0.25;
+  }
+} 
+const justSub = subscriptionOnly(sarah.pricePerRefill, sarah.refills);
+
+
+
+console.log("Your grand total is $", justSub);
 
 //function for coupon
 
-//function for both subscription and coupon
+function withCoupon(costPer, numRefills) {
+  if (timmy.coupon == true) {
+    return costPer * numRefills - 10;
+  }
+}
+
+const justCoupon = withCoupon(timmy.pricePerRefill, timmy.refills);
+
+console.log("Your grand total is $",justCoupon);
+
+//function for both subscription and coupon.
+
+function hasBoth(costPer, numRefills) {
+    if (rocky.coupon == true && rocky.subscription == true) {
+        return (costPer * numRefills - costPer * numRefills * 0.25) - 10;
+
+    }
+    
+}
+const doubleDeal = hasBoth(rocky.pricePerRefill, rocky.refills);
+console.log("Your grand total is $",doubleDeal);
+// }else {
+//     return costPer * numRefills;
